934d35d5fa5fa80396c776a01766cfb8be3db3c0
Mikael <copemikael@gmail.com>
2024-01-14 19:51:06 +0200

    Initital setup of ReadMe Generator

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Node-README
 A Node driven JS file that creates a dynamic README file.
+
+# Heading level 1
+
+## Heading level 2
---
+++ index.js
@@ -0,0 +1,69 @@
+const fs = require("fs");
+const path = require('path');
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// array of questions for user
+const questions = [
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?',
+    },
+    {
+      type: 'input',
+      name: 'discription',
+      message: 'What is a discription of your project?',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'What are your installation instructions?',
+    },
+    {
+      type: 'input',
+      name: 'usageInfo',
+      message: 'What is your usage information?',
+    },
+    {
+      type: 'input',
+      name: 'contribution',
+      message: 'What are your contribution guidelines?',
+    },
+    {
+      type: 'input',
+      name: 'testInstruct',
+      message: 'What are your test instructions?',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Which license would you like to select?',
+      choices: ['Apache 2.0 License', 'Boost Software License 1.0', 'The MIT License'],
+    },
+    {
+      type: 'input',
+      name: 'gitHub',
+      message: 'What is your github username?',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your email address?',
+    },
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+ inquirer.prompt(questions)
+ .then((answers) => writeFileAsync('readme.md', generateMarkdown(answers)))
+  .then(() => console.log('Successfully wrote to readme.md'))
+  .catch((err) => console.error(err));
+}
+
+// function call to initialize program
+init();
